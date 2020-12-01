e8af7556286b4875095e84e44ff3f8c63bb4c8a3
CSE <revengerreigns076@gmail.com>
2020-12-01 22:20:51 +0600

    Variables & input in python

---
+++ Variables.py
@@ -0,0 +1,6 @@
+# takin input & storing into a variable
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number:  '))
+# printing output
+print('The summation is ', (num1+num2))
+print('The summation is '+str((num1+num2)))
