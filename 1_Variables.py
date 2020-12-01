34bb59955a224204f0990d3f9609f64bd3458a80
CSE <revengerreigns076@gmail.com>
2020-12-01 23:29:51 +0600

    Modifying Variables code

---
+++ 1_Variables.py
@@ -0,0 +1,16 @@
+# takin input & storing into a variable
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number:  '))
+# printing output
+print('The summation is ', (num1+num2))
+print('The summation is '+str((num1+num2)))  # type casting
+
+# in python there is no need to say datatype explicitely
+a = 1
+b = 'Ruman'
+c = 24.5
+d = True
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
+print(type(d), d)
