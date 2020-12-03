362e0e934fac7285ae58b92f48405abe7092a20c
CSE <revengerreigns076@gmail.com>
2020-12-03 11:52:47 +0600

    List Comprehension

--- 14_lambdaFunction.py
+++ 14_lambdaFunction.py
@@ -5,3 +5,18 @@ def cube(x): return x*x*x
 
 print(f'Square of 2: {square(2)}')
 print(f'Cube of 2 : {cube(2)}')
+# list comprehension in single loop
+
+
+def myfunc(num):
+    return num % 2 == 0
+
+
+numbers = [num for num in range(1, 6)]
+doubles = [num*2 for num in numbers]
+evenNumbers = filter(myfunc, numbers)
+oddNumbers = filter(lambda x: x % 2, numbers)
+
+print(f'Double numbers: {doubles}')
+print(f'Even numbers: {list(evenNumbers)}')
+print(f'Odd numbers: {list(oddNumbers)}')
