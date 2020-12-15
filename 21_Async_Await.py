6ab2efbd45a01b3f7ecf92c7698bb29c4c5c93bb
CSE <revengerreigns076@gmail.com>
2020-12-15 10:07:27 +0600

    Async & Await in python

---
+++ 21_Async_Await.py
@@ -0,0 +1,15 @@
+
+import asyncio
+import time
+
+
+async def PrintSomething():
+    print("Hello ")
+    # asynchronous way of sleeping, await functionName/operation will do
+    await asyncio.sleep(2)
+    print("World")
+
+asyncio.run(PrintSomething())
+# synchronous way of sleeping
+time.sleep(2)
+print("How are you ?")
